6bdb10eebb877da92a66b50a4a805bcfe34c53fb
Kei Thoma | JKM <thoma@jkm-consult.de>
2026-03-21 14:46:21 +0100

    still testing rust out

--- DISCRETE-CONCRETE/examples/basic.rs
+++ DISCRETE-CONCRETE/examples/basic.rs
@@ -6,17 +6,13 @@ struct Euclidean;
 
 impl GCDAlgorithm for Euclidean {
     fn gcd(mut a: u64, mut b: u64) -> u64 {
-        while b != 0 {
-            a %= b;
-            std::mem::swap(&mut a, &mut b);
-        }
         a
     }
 }
 
 fn main() {
-    let a: u64 = 64;
-    let b: u64 = 24;
-    let g = Euclidean::gcd(a, b);
-    println!("{}", g);
+    let a: u64 = 17;
+    let b: u64 = 2;
+    let n: u64 = b % a;
+    println!("{}", n);
 }
