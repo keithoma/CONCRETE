aea70ac941707b777405241510e5292d8d45afa5
keithoma <thomakmj@gmail.com>
2026-03-21 10:31:13 +0100

    RNG is a bit too hard, pivoting to rational

--- RNG-CONCRETE/examples/basic.rs
+++ RNG-CONCRETE/examples/basic.rs
@@ -1,4 +1,15 @@
-use RNGCONCRETE::{Unit, standalone_hello}; // This pulls from your lib.rs
+trait Next {
+    fn step(&mut self)
+}
+
+
+struct State {
+    current: u64
+}
+
+
+
+// implement all different kind of Next
 
 fn main() {
     standalone_hello()
