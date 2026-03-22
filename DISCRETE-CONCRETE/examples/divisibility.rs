852f584feb5ca5ce9963d5946bf6cbec4b0e74f8
keithoma <thomakmj@gmail.com>
2026-03-22 07:42:37 +0100

    cleaned up the file

--- DISCRETE-CONCRETE/examples/divisibility.rs
+++ DISCRETE-CONCRETE/examples/divisibility.rs
@@ -76,14 +76,14 @@ mod digits {
 }
 
 impl DivisibilityByMethodTrait for DivisibleBy2Method {
-    fn check(&self, x: u64) -> bool {
+    fn check(&self, n: u64) -> bool {
         match self {
             DivisibleBy2Method::LastDigitEven => {
-                matches!(x % 10, 0 | 2 | 4 | 6 | 8)
+                matches!(n % 10, 0 | 2 | 4 | 6 | 8)
             }
 
             DivisibleBy2Method::ModuloOperator => {
-                x % 2 == 0
+                n % 2 == 0
             }
         }
     }
@@ -169,12 +169,12 @@ impl DivisibilityByMethodTrait for DivisibleBy4Method {
                 }
             }
 
-            DivisibilityBy4Method::TwiceTensPlusOnes => {
-                if x < 10 {
+            DivisibleBy4Method::TwiceTensPlusOnes => {
+                if n < 10 {
                     crate::digits::divisible_by_4(n)
                 } else {
                     let intermediate_result: u64 = 2 * ((n % 100) / 10) + (n % 10);
-                    intermediate_result.divisible_by_with(Di)
+                    intermediate_result.divisible_by_with(DivisibleBy4Method::TwiceTensPlusOnes)
                 }
             }
 
@@ -193,35 +193,8 @@ impl Divisible for u64 {
     }
 }
 
-
-
-
-
-    fn divisible_by_4(self) -> bool {
-        matches!(self % 100, 0 | 4 | 8 | 12 | 16 | 20 | 24 | 28 | 32 | 36 | 40 | 44 | 48 | 52 | 56 | 60 | 64 | 68 | 72 | 76 | 80 | 84 | 88 | 92 | 96 )
-    }
-
-    fn divisible_by_5(self) -> bool {
-        matches!(self % 10, 0 | 5)
-    }
-
-    fn divisible_by_6(self) -> bool {
-        self.divisible_by_2() && self.divisible_by_3()
-    }
-
-    fn divisible_by_9(self) -> bool {
-        if self < 10 {
-            self == 9
-        } else {
-            digit_sum(self).divisible_by_9()
-        }
-    }
-
-    fn divisible_by_10(self) -> bool {
-        self % 10 == 0
-    }
-}
-
 fn main() {
-    println!("{:?}", 369.divisible_by_3())
+    println!("{:?}", 369.divisible_by_with(DivisibleBy3Method::DigitSum));
+    println!("{:?}", 369.divisible_by_with(DivisibleBy3Method::DigitClassCount));
+    println!("{:?}", 369.divisible_by_with(DivisibleBy3Method::SubtractDoubleLastDigit));
 }
