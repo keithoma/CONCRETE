0c5df3f181bf45c0e09d6d5686f0caf81a518459
keithoma <thomakmj@gmail.com>
2026-03-22 11:46:53 +0100

    minimal working version

--- DISCRETE-CONCRETE/examples/digits.rs
+++ DISCRETE-CONCRETE/examples/digits.rs
@@ -2,3 +2,39 @@
 struct Digits {
     digits: Vec<u8>,
 }
+
+impl Digits {
+    /// Returns the number of digits.
+    fn digit_count(n: u64) -> u64 {
+        if n == 0 {
+            1
+        } else {
+            self.ilog10() + 1
+        }
+    }
+
+    /// Constructs a new empty Digits instance.
+    fn new() -> Self {
+        Self {
+            digits: Vec::new(),
+        }
+    }
+
+    /// Constructs and populates new Digits instance with the digits from `n`.
+    fn from_u64(n: u64) -> Self {
+        if n == 0 { return vec![0] }
+
+        let mut n = n;
+        let mut digits = Vec::new();
+        while n > 0 {
+            digits.push((n % 10) as u8);
+            n /= 10;
+        }
+        digits
+    }
+}
+
+fn main() {
+    let d = Digits::from_u64(123456);
+    println!("{:?}", d::digits)
+}
