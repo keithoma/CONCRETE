079b32bf034db01402f5f8b5e0801206f040787c
Kei Thoma | JKM <thoma@jkm-consult.de>
2026-03-27 20:15:49 +0100

    it's friday

---
+++ DISCRETE-CONCRETE/examples/prime_refactor.rs
@@ -0,0 +1,26 @@
+/*
+fn is_prime(n: u64) -> bool;
+fn primes_up_to(n: u64) -> array;
+
+implementations to find out if it's prime
+
+TrialDivision
+
+implementations for primes_up_to_n
+
+the sieve of this greek guy
+
+(each implementation of is_prime gives method for primes up_to)
+
+
+also include the version we just look up a hash table
+fn create table
+fn look up table
+
+*/
+
+/*
+functions for proof of concept
+fn array_needed(type: integer_type) -> integer_type;
+
+*/
