c74df02f146ba6dfa93cdb5518cfb4896d3cbeea
Kei Thoma <thomakmj@gmail.com>
2026-04-06 21:31:38 +0200

    implemented trial division

--- algebra_concrete/src/number_theory/is_prime.rs
+++ algebra_concrete/src/number_theory/is_prime.rs
@@ -6,23 +6,31 @@ use crate::structures::integer::{Integer};
 /// strategy for iterative euclidean algorithm
 #[derive(Debug)] #[non_exhaustive] pub struct TrialDivision;
 
+/// primes
 pub trait PrimeAlgorithm<T: Integer> {
     /// computes the gcd
     fn is_prime(n: T) -> bool;
 
-    // maybe primes_up+to here?
+    // maybe primes_up_to here?
 }
 
 impl<T: Integer> PrimeAlgorithm<T> for TrialDivision {
     fn is_prime(n: T) -> bool {
-        // 0 and 1 are not primes by convention.
-        if n <= 1 { return false; }
+        #![expect(
+            clippy::arithmetic_side_effects, 
+            reason = "none, maybe they break idk"
+        )]
 
-        for i in 2..n {
-            // We only need to check divisibility to the square root of n
-            // rounded down. We are also avoiding float numbers here.
-            if i > n / i { break; }
-            if n % i == 0 { return false; }
+        // 0 and 1 are not primes by convention.
+        if n == T::ZERO || n == T::ONE { return false; }
+
+        // We iterate from 2 to the the square root of `n` rounded down.
+        let mut i = T::ONE + T::ONE;
+        while i <= n / i {
+            if n % i == T::ZERO {
+                return false;
+            }
+            i += T::ONE;
         }
         true
     }
