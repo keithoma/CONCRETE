f56bed063195c962350d57899e8fdf36c80ab7a9
Kei Thoma <thomakmj@gmail.com>
2026-04-23 12:39:13 +0200

    change logic so that we take usize

--- algebra_concrete/src/number_theory/sieve_of_eratosthenes.rs
+++ algebra_concrete/src/number_theory/sieve_of_eratosthenes.rs
@@ -2,6 +2,7 @@
 
 use alloc::vec;
 use alloc::vec::Vec;
+use core::convert::TryFrom;
 
 use crate::{structures::integer::Integer};
 
@@ -16,34 +17,41 @@ pub trait PrimeAlgorithm2<T: Integer> {
     fn primes_up_to(n: T) -> Vec<T>;
 }
 
-impl<T: Integer> PrimeAlgorithm2<T> for SieveOfEratosthenes {
+impl<T> PrimeAlgorithm2<T> for SieveOfEratosthenes
+where
+    T: Integer + TryFrom<usize>,
+    usize: TryFrom<T>,
+{
     fn primes_up_to(n: T) -> Vec<T> {
         // 0 and 1 are not primes by convention.
         if n == T::ZERO || n == T::ONE { return Vec::new() }
-        let vec_size = (n - T::ONE).to_usize();
 
-        let mut number_line = vec![true; vec_size]; // we check from 2 to n
-        let two = T::ONE + T::ONE; // just a helper constant
-        let mut i = two; // we start from 2
+        let n_usize: usize = n.to_usize();
+        let mut number_line = vec![true; n_usize - 1]; // we check from 2 to n
+        let mut i = 2; // we start from 2
 
         // we need only to check up to the floor of sqrt(n)
-        while i <= n / i {
+        while i <= n_usize / i {
             // remember that the index of the vector has an offset of 2
-            if number_line[i - two] {
-                let j = i;
-                while j < n + T::ONE {
-                    number_line[j] = false;
+            if number_line[i - 2] {
+                let j = i * i;
+                while j <= n_usize {
+                    number_line[j - 2] = false;
                     j += i;
                 }
             }
 
-            i += T::ONE;
+            i += 1;
         }
 
-        return number_line.iter()
+        number_line.iter()
             .enumerate()
             .filter(|(_, &val)| val)
-            .map(|(i, _)| i + two)
+            .map(|(index, _)| {
+                let prime_usize = index + 2;
+                // Convert back from usize to T
+                T::try_from(prime_usize).ok().expect("Value fits in usize but not T")
+            })
             .collect()
     }
 
